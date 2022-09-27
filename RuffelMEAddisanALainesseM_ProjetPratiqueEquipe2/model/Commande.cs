acbcccb7523412e148caece0d1672a08281928e6
Maxime Lainesse <0684941@bdeb.qc.ca>
2022-09-27 08:23:45 -0400

    Base model Commande

--- RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/model/Commande.cs
+++ RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/model/Commande.cs
@@ -2,5 +2,34 @@
 
 public class Commande
 {
-    
+    private int noCommande;
+    private DateTime dateCommande;
+    private int noClient;
+
+    public Commande(DateTime dateCommande, int noClient)
+    {
+        this.dateCommande = dateCommande;
+        this.noClient = noClient;
+    }
+
+    public Commande(int noCommande, DateTime dateCommande, int noClient)
+    {
+        this.noCommande = noCommande;
+        this.dateCommande = dateCommande;
+        this.noClient = noClient;
+    }
+
+    public int NoCommande => noCommande;
+
+    public DateTime DateCommande
+    {
+        get => dateCommande;
+        set => dateCommande = value;
+    }
+
+    public int NoClient
+    {
+        get => noClient;
+        set => noClient = value;
+    }
 }
