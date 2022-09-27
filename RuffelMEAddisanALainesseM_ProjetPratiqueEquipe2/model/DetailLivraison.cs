dd62a3b976987fbb444c87c0564cb920705f48f0
Maxime Lainesse <0684941@bdeb.qc.ca>
2022-09-27 08:27:27 -0400

    Base model DetailLivraison

--- RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/model/DetailLivraison.cs
+++ RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/model/DetailLivraison.cs
@@ -2,5 +2,35 @@
 
 public class DetailLivraison
 {
-    
+    private int noDetailLivraison;
+    private int noCommande;
+    private int noArticle;
+    private int quantiteLivree;
+
+    public DetailLivraison(int noCommande, int noArticle, int quantiteLivree)
+    {
+        this.noCommande = noCommande;
+        this.noArticle = noArticle;
+        this.quantiteLivree = quantiteLivree;
+    }
+
+    public DetailLivraison(int noDetailLivraison, int noCommande, int noArticle, int quantiteLivree)
+    {
+        this.noDetailLivraison = noDetailLivraison;
+        this.noCommande = noCommande;
+        this.noArticle = noArticle;
+        this.quantiteLivree = quantiteLivree;
+    }
+
+    public int NoDetailLivraison => noDetailLivraison;
+
+    public int NoCommande => noCommande;
+
+    public int NoArticle => noArticle;
+
+    public int QuantiteLivree
+    {
+        get => quantiteLivree;
+        set => quantiteLivree = value;
+    }
 }
