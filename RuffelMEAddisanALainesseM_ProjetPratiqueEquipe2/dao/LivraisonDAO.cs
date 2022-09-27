0395238e8e4fa4380a3ee05e9734b6a0dd661f76
addisan <addisan.anthony@gmail.com>
2022-09-27 10:45:48 -0400

    correction

--- RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/dao/LivraisonDAO.cs
+++ RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/dao/LivraisonDAO.cs
@@ -3,17 +3,18 @@ public class LivraisonDAO
 {
 	public LivraisonDAO()
 	{
-		InsertLivraison(Livraison livraison)
-		{
-			OracleConnection connection = DBConnection.GetInstance();
-			OracleCommand cmd = new OracleCommand();
-			cmd.Connection = connection;
-			cmd.CommandText = "INSERT INTO VENTE.LIVRAISON VALUES (:NOLIVRAISON, :DATELIVRAISON");
-			cmd.Parameters.Add(new OracleParameter("NOLIVRAISON", livraison.NoLivraison));
-			cmd.Parameters.Add(new OracleParameter("DATELIVRAISON", livraison.DateLivraison));
-			connection.Open();
-			cmd.ExecuteNonQuery();
-			connection.Close();
-		}
+	}
+	
+	void InsertLivraison(Livraison livraison)
+	{
+		OracleConnection connection = DBConnection.GetInstance();
+		OracleCommand cmd = new OracleCommand();
+		cmd.Connection = connection;
+		cmd.CommandText = "INSERT INTO VENTE.LIVRAISON VALUES (:NOLIVRAISON, :DATELIVRAISON");
+		cmd.Parameters.Add(new OracleParameter("NOLIVRAISON", livraison.NoLivraison));
+		cmd.Parameters.Add(new OracleParameter("DATELIVRAISON", livraison.DateLivraison));
+		connection.Open();
+		cmd.ExecuteNonQuery();
+		connection.Close();
 	}
 }
