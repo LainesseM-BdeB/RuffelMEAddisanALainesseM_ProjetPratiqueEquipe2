69b5c6e81beab78df15729534c7bbac52ffea85d
addisan <addisan.anthony@gmail.com>
2022-09-27 10:56:59 -0400

    move connection from inside method -> attribute

--- RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/dao/LivraisonDAO.cs
+++ RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/dao/LivraisonDAO.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 public class LivraisonDAO
 {
+	OracleConnection connection = DBConnection.GetInstance();
 	public LivraisonDAO()
 	{
 	}
 	
 	void InsertLivraison(Livraison livraison)
 	{
-		OracleConnection connection = DBConnection.GetInstance();
 		OracleCommand cmd = new OracleCommand();
 		cmd.Connection = connection;
 		cmd.CommandText = "INSERT INTO VENTE.LIVRAISON VALUES (:NOLIVRAISON, :DATELIVRAISON");
 		cmd.Parameters.Add(new OracleParameter("NOLIVRAISON", livraison.NoLivraison));
 		cmd.Parameters.Add(new OracleParameter("DATELIVRAISON", livraison.DateLivraison));
-		connection.Open();
-		cmd.ExecuteNonQuery();
-		connection.Close();
+		try
+		{
+			connection.Open();
+			cmd.ExecuteNonQuery();
+
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.Message);
+		}
+		finally
+		{
+			connection.Close();
+		}
+		
+		
 	}
 }
