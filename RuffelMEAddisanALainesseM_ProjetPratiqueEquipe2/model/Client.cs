ca21f3cb491b0afd8cc0b956784be3b1eed0067e
Maxime Lainesse <0684941@bdeb.qc.ca>
2022-09-27 08:19:04 -0400

    Base model client

--- RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/model/Client.cs
+++ RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/model/Client.cs
@@ -2,5 +2,34 @@
 
 public class Client
 {
-    
+    private int noClient;
+    private string nomClient;
+    private string noTelephone;
+
+    public Client(string nomClient, string noTelephone)
+    {
+        this.nomClient = nomClient;
+        this.noTelephone = noTelephone;
+    }
+
+    public Client(int noClient, string nomClient, string noTelephone)
+    {
+        this.noClient = noClient;
+        this.nomClient = nomClient;
+        this.noTelephone = noTelephone;
+    }
+
+    public string NomClient
+    {
+        get => nomClient;
+        set => nomClient = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string NoTelephone
+    {
+        get => noTelephone;
+        set => noTelephone = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int NoClient => noClient;
 }
