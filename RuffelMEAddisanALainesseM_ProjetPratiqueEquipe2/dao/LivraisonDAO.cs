5f3c69b5c3251e44c9749782f7ea3ba907775bc7
addisan <addisan.anthony@gmail.com>
2022-09-27 10:41:11 -0400

    InsertLivraison

--- RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/dao/LivraisonDAO.cs
+++ RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/dao/LivraisonDAO.cs
@@ -5,7 +5,15 @@ public class LivraisonDAO
 	{
 		InsertLivraison(Livraison livraison)
 		{
-			
+			OracleConnection connection = DBConnection.GetInstance();
+			OracleCommand cmd = new OracleCommand();
+			cmd.Connection = connection;
+			cmd.CommandText = "INSERT INTO VENTE.LIVRAISON VALUES (:NOLIVRAISON, :DATELIVRAISON");
+			cmd.Parameters.Add(new OracleParameter("NOLIVRAISON", livraison.NoLivraison));
+			cmd.Parameters.Add(new OracleParameter("DATELIVRAISON", livraison.DateLivraison));
+			connection.Open();
+			cmd.ExecuteNonQuery();
+			connection.Close();
 		}
 	}
 }
