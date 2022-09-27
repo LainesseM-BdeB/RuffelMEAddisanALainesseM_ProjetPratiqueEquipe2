15be1b31f9b001aba5bd4ab3ca15cbced5084064
addisan <addisan.anthony@gmail.com>
2022-09-27 12:14:13 -0400

    new method InsertLigneCommande

--- RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/dao/LigneCommandeDAO.cs
+++ RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/dao/LigneCommandeDAO.cs
@@ -8,6 +8,28 @@ public class LigneCommandeDAO
 		
 	}
 	
+	public void InsertLigneCommande(LigneCommande ligneCommande)
+	{
+		OracleCommand cmd = new OracleCommand();
+		cmd.Connection = connection;
+		cmd.CommandText = "INSERT INTO VENTE.LIGNECOMMANDE VALUES (:NOCOMMANDE, :NOARTICLE, :QUANTITE")";
+		cmd.Parameters.Add(new OracleParameter("NOCOMMANDE", ligneCommande.NoCommande));
+		cmd.Parameters.Add(new OracleParameter("NOARTICLE", livraison.NoArticle));
+		cmd.Parameters.Add(new OracleParameter("QUANTITE", livraison.Quantite));
+		try
+		{
+			connection.Open();
+			cmd.ExecuteNonQuery();
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.Message);
+		}
+		finally
+		{
+			connection.Close();
+		}
+	}
 	
 	
 }
