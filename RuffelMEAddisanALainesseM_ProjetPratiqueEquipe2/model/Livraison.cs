9a6df5b47ca1008b9813de190b336faa1535f23a
Maxime Lainesse <0684941@bdeb.qc.ca>
2022-09-27 08:25:49 -0400

    Base model Livraison

--- RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/model/Livraison.cs
+++ RuffelMEAddisanALainesseM_ProjetPratiqueEquipe2/model/Livraison.cs
@@ -2,5 +2,25 @@
 
 public class Livraison
 {
-    
+    private int noLivraison;
+    private DateTime dateLivraison;
+
+    public Livraison(DateTime dateLivraison)
+    {
+        this.dateLivraison = dateLivraison;
+    }
+
+    public Livraison(int noLivraison, DateTime dateLivraison)
+    {
+        this.noLivraison = noLivraison;
+        this.dateLivraison = dateLivraison;
+    }
+
+    public int NoLivraison => noLivraison;
+
+    public DateTime DateLivraison
+    {
+        get => dateLivraison;
+        set => dateLivraison = value;
+    }
 }
